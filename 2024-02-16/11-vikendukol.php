efaff298b63c04ba7e8c1c07cb4b4f75ae3c33d0
Marek Bis <marek.bis5@seznam.cz>
2024-02-18 13:12:07 +0100

    ukol hotovo

--- 2024-02-16/11-vikendukol.php
+++ 2024-02-16/11-vikendukol.php
@@ -1,23 +1,42 @@
 <?php
-    if (array_key_exists("submit-kolecka",$_POST)){
-        $pocetKolecek = $_POST["pocet-kolecek"]:
-        echo $pocetKolecek;
-    }
+$pocetKolecek = 0;
+if (array_key_exists("submit-kolecka", $_POST)) {
+    $pocetKolecek = $_POST["pocet-kolecek"];
+    $rand = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f');
+}
 
 
 ?>
-
-
-
 <!-- formular, kam uzivatel zada nejake cislo  -->
 <!-- vygenerujte mu zadany pocet kolecek kazde kolecko bude mit nahodnou barvu -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Balls</title>
+    <style>
+        body {
+            text-align: center;
+        }
+
+        .container {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .ball {
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+            margin: 2rem;
+        }
+    </style>
 </head>
+
 <body>
     <h1>ukol</h1>
     <form action="" method="post">
@@ -25,6 +44,14 @@
         <input type="number" name="pocet-kolecek" id="">
         <input type="submit" name="submit-kolecka" value="Odeslat">
     </form>
-    
+    <div class="container">
+        <?php
+        for ($i = 0; $i < $pocetKolecek; $i++) {
+            $color = '#' . $rand[rand(0, 15)] . $rand[rand(0, 15)] . $rand[rand(0, 15)] . $rand[rand(0, 15)] . $rand[rand(0, 15)] . $rand[rand(0, 15)];
+            echo "<div class=ball style=background:$color> </div>";
+        }
+        ?>
+    </div>
 </body>
+
 </html>
