3e5d1e5b40fb1ea3fe21c7ccdf5e8d6fe184ed5b
Marek Bis <marek.bis5@seznam.cz>
2024-02-18 12:42:38 +0100

    test commit ukolu

--- 2024-02-16/11-vikendukol.php
+++ 2024-02-16/11-vikendukol.php
@@ -1,3 +1,14 @@
+<?php
+    if (array_key_exists("submit-kolecka",$_POST)){
+        $pocetKolecek = $_POST["pocet-kolecek"]:
+        echo $pocetKolecek;
+    }
+
+
+?>
+
+
+
 <!-- formular, kam uzivatel zada nejake cislo  -->
 <!-- vygenerujte mu zadany pocet kolecek kazde kolecko bude mit nahodnou barvu -->
 <!DOCTYPE html>
@@ -8,6 +19,12 @@
     <title>Document</title>
 </head>
 <body>
+    <h1>ukol</h1>
+    <form action="" method="post">
+        <label for="">Zadejte pocet kolecek: </label>
+        <input type="number" name="pocet-kolecek" id="">
+        <input type="submit" name="submit-kolecka" value="Odeslat">
+    </form>
     
 </body>
 </html>
